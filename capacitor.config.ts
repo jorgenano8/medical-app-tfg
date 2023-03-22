d44deb91cdab307485b74e386be53abe7f888617
Jorge Navarro Nogueroles <jorgenano8@gmail.com>
2023-03-22 20:38:48 +0100

    Commit inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'medical.app.tfg',
+  appName: 'medical-app-tfg',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
