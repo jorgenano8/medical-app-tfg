a7c1fcc9b761497eb9a6014207b420e8613b6a4f
Jorge Navarro Nogueroles <jorgenano8@gmail.com>
2023-05-10 19:22:09 +0200

    Carga de ultimas publicaciones (por mejorar)

--- src/app/paginas/inicio/inicio.page.ts
+++ src/app/paginas/inicio/inicio.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Publicacion } from 'src/app/core/modelos/publicacion.model';
+import { PublicacionService } from 'src/app/core/servicios/publicacion.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  public listaPublicaciones: Publicacion[]=[];
+
+  constructor(private publicacionService: PublicacionService) { }
 
   ngOnInit() {
+    this.publicacionService.getPublicaciones().valueChanges().subscribe((publicaciones)=>{
+      this.listaPublicaciones=publicaciones.map((publicacion)=>{
+        return new Publicacion(publicacion.uid, publicacion.usuario, publicacion.fechaPublicacion, publicacion.etiqueta, publicacion.titulo, publicacion.contenido);
+      });
+      console.log(this.listaPublicaciones);
+      this.ordenarPublicaciones();
+    });
+  }
+
+  ordenarPublicaciones(){
+    this.listaPublicaciones.sort((a, b) => {
+      const dateA = a.fechaPublicacion ? new Date(a.fechaPublicacion) : undefined;
+      const dateB = b.fechaPublicacion ? new Date(b.fechaPublicacion) : undefined;
+      if (dateA && dateB) {
+        return dateB.getTime() - dateA.getTime();
+      }
+      return dateB ? 1 : -1;
+    });
   }
 
 }
