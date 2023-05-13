3784aeb538077a0966611b299a4d6e287ddcfdad
Jorge Navarro Nogueroles <jorgenano8@gmail.com>
2023-05-13 15:13:34 +0200

    FEATURE: página de una publicación

--- src/app/paginas/publicacion/publicacion.page.ts
+++ src/app/paginas/publicacion/publicacion.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Publicacion } from 'src/app/core/modelos/publicacion.model';
+import { Usuario } from 'src/app/core/modelos/usuario.model';
+import { PublicacionService } from 'src/app/core/servicios/publicacion.service';
+import { UsuarioService } from 'src/app/core/servicios/usuario.service';
 
 @Component({
   selector: 'app-publicacion',
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicacionPage implements OnInit {
 
-  constructor() { }
+  public publicacionModel: Publicacion={};
+  public usuarioModel: Usuario={};
+
+  public loaded = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private publicacionService: PublicacionService,
+    private usuarioService: UsuarioService) { }
 
   ngOnInit() {
+    console.log("ngOnInit")
+    this.loaded=false;
+    this.cargarPublicacion(this.route.snapshot.params['uid']);
+  }
+
+  ionViewWillEnter(){
+  }
+
+  cargarPublicacion(uidPublicacion: String){
+    this.publicacionService.getPublicacion(uidPublicacion).subscribe((infoPublicacion) => {
+      this.rellenarDatosPublicacion(infoPublicacion.data());
+      this.cargarUsuario(this.publicacionModel.usuario);
+    })
+  }
+
+  rellenarDatosPublicacion(infoPublicacion: any){
+    this.publicacionModel.usuario = infoPublicacion.usuario;
+    this.publicacionModel.fechaPublicacion = infoPublicacion.fechaPublicacion;
+    this.publicacionModel.etiqueta = infoPublicacion.etiqueta;
+    this.publicacionModel.titulo = infoPublicacion.titulo;
+    this.publicacionModel.contenido = infoPublicacion.contenido;
+  }
+
+  cargarUsuario(uidUsuario: any){
+    this.usuarioService.getUsuario(uidUsuario).subscribe((infoUsuario) => {
+      this.rellenarDatosUsuario(infoUsuario.data());
+    })
+  }
+
+  rellenarDatosUsuario(infoUsuario: any){
+    this.usuarioModel.nombre = infoUsuario.nombre;
+    this.usuarioModel.apellidos = infoUsuario.apellidos;
+    this.loaded=true;
+
   }
 
 }
