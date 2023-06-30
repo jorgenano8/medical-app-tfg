b0f7625945cae5449609c0fe99d73d60c6d8bd61
Jorge Navarro Nogueroles <jorgenano8@gmail.com>
2023-06-30 15:27:40 +0200

    Lógica pag. inicio

--- src/app/paginas/inicio/inicio.page.ts
+++ src/app/paginas/inicio/inicio.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 import { Publicacion } from 'src/app/core/modelos/publicacion.model';
+import { AuthService } from 'src/app/core/servicios/auth.service';
 import { PublicacionService } from 'src/app/core/servicios/publicacion.service';
+import { UsuarioService } from 'src/app/core/servicios/usuario.service';
 
 @Component({
   selector: 'app-inicio',
@@ -10,14 +14,45 @@ import { PublicacionService } from 'src/app/core/servicios/publicacion.service';
 export class InicioPage implements OnInit {
 
   public listaPublicaciones: Publicacion[]=[];
+  public uidUsuarios: any[] = [];
+  public loaded :  Boolean = false;
 
-  constructor(private publicacionService: PublicacionService) { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private publicacionService: PublicacionService,
+    private afAuth: AngularFireAuth,
+    private authService: AuthService,
+    private router: Router,) { }
 
   ngOnInit() {
   }
 
   ionViewWillEnter(){
-    console.log("ionViewWillEnter");
+    this.uidUsuarios=[];
+    this.listaPublicaciones=[];
+    this.cargarSeguidos();
   }
 
+  cargarSeguidos(){
+    this.loaded = false;
+    this.afAuth.onAuthStateChanged((currentUser)=>{
+      if(!currentUser){ return; }
+
+      this.usuarioService.getUsuario(currentUser?.uid).subscribe((infoUsuario) => {
+        this.uidUsuarios.push(currentUser.uid);
+        infoUsuario.data()?.seguidos?.forEach(seguido=>{
+        this.uidUsuarios.push(seguido);
+        })
+
+        this.publicacionService.getPublicaciones().ref.where('usuario', 'in', this.uidUsuarios).orderBy('dateSystem', 'desc').get().then((publicaciones)=>{
+          publicaciones.forEach(publicacion=>{
+            this.listaPublicaciones.push(publicacion.data());
+          })
+        })
+
+      })
+    }).catch((error)=>{
+      console.log(error.message);
+    });
+  }
 }
