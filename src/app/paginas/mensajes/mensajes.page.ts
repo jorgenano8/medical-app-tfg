b36f0ba1d1410ca148dc2c46b0a720276caab383
Jorge Navarro Nogueroles <jorgenano8@gmail.com>
2023-07-09 13:51:14 +0200

    FEATURE: chats. página mensajes

--- src/app/paginas/mensajes/mensajes.page.ts
+++ src/app/paginas/mensajes/mensajes.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Chat } from 'src/app/core/modelos/chat.model';
+import { Usuario } from 'src/app/core/modelos/usuario.model';
 import { ChatService } from 'src/app/core/servicios/chat.service';
 import { UsuarioService } from 'src/app/core/servicios/usuario.service';
 
@@ -12,7 +13,9 @@ import { UsuarioService } from 'src/app/core/servicios/usuario.service';
 export class MensajesPage implements OnInit {
 
   public listaChats: Chat[]=[];
+  public listaReceptores: any[]=[];
   public userUIDLogged:any;
+  public loaded: Boolean=false;
 
   constructor(
     private chatService: ChatService,
@@ -25,9 +28,11 @@ export class MensajesPage implements OnInit {
 
   ionViewWillEnter(){
     this.resetPagina();
+    this.loaded=false;
     this.afAuth.authState.subscribe((user) => {
       if(user){
-        this.cargarChats(user.uid);
+        this.userUIDLogged=user.uid;
+        this.cargarChats();
       }
     });
   }
@@ -38,21 +43,38 @@ export class MensajesPage implements OnInit {
 
   resetPagina(){
     this.listaChats=[];
+    this.listaReceptores=[]
     this.userUIDLogged='';
   }
 
-  cargarChats(uid:any){
-    this.chatService.getChats().ref.where("usuario1", "==", uid).get().then((listaUsuario1)=>{
-      listaUsuario1.forEach(chatUsuario1=>{
-        this.listaChats.push(chatUsuario1.data())
+  cargarChats(){
+    this.chatService.getChats().ref.where("usuario1", "==", this.userUIDLogged).get().then((listaUsuario1)=>{
+      listaUsuario1.forEach(chat=>{
+        this.listaChats.push(chat.data())
       })
     }).then(()=>{
-      this.chatService.getChats().ref.where("usuario2", "==", uid).get().then((listaUsuario1)=>{
-        listaUsuario1.forEach(chatUsuario1=>{
-          this.listaChats.push(chatUsuario1.data())
+      this.chatService.getChats().ref.where("usuario2", "==", this.userUIDLogged).get().then((listaUsuario2)=>{
+        listaUsuario2.forEach(chat=>{
+          this.listaChats.push(chat.data())
         })
       })
-    })
+    }).then(()=>{
+      this.listaChats.forEach(chat=>{
+        if(chat.usuario1==this.userUIDLogged){
+          this.usuarioService.getUsuario(chat.usuario2).subscribe((user)=>{
+            this.listaReceptores.push(user.data());
+          })
+        }
+        if(chat.usuario2==this.userUIDLogged){
+          this.usuarioService.getUsuario(chat.usuario1).subscribe((user)=>{
+            this.listaReceptores.push(user.data());
+          })
+        }
+      })
+      this.loaded=true;
+    }).catch((error)=>{
+      console.log(error.message);
+    });
   }
 
   crearChat(){
